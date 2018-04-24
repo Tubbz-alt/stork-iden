a025d46b1370a1e91ec34c3963e1d0feaaac978c
Phisit Kannasut <phisit@readyplanet.com>
2018-04-24 13:40:33 +0700

    add email reset Password

--- libs/email.js
+++ libs/email.js
@@ -21,5 +21,26 @@ module.exports = {
         .then(reslove)
         .catch(reject);
     });
+  },
+  resetPassword: function (receiver, to, resetUrl, lang) {
+    var that = this;
+    return new Promise(function (reslove, reject) {
+      var method = '/apis/email/resetpassword';
+      var options = {
+        method: 'POST',
+        url: that.HOSTNAME + method,
+        headers: {
+          'Cache-Control': 'no-cache',
+          Authorization: that.AUTH,
+          'x-project': that.project_id,
+          'Content-Type': 'application/json'
+        },
+        data: {receiver, to, resetUrl, lang }
+      };
+
+      res = axios(options)
+        .then(reslove)
+        .catch(reject);
+    });
   }
 }
