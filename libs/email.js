2fcdafd1b8a2daabe40b2589ca5b68fb9321a293
Phisit Kannasut <phisit@readyplanet.com>
2018-05-02 15:36:46 +0700

    add email invite, change permission

--- libs/email.js
+++ libs/email.js
@@ -2,11 +2,21 @@ module.exports = {
   validator: function (receiver, to, validateUrl, lang) {
     var method = '/apis/email/validator';
     return require('./axiosRequest')(this.project_id, this.AUTH, this.HOSTNAME + method,
-      { receiver, to, validateUrl, lang })
+      { receiver, to, validateUrl, lang });
   },
   resetPassword: function (receiver, to, resetUrl, lang) {
     var method = '/apis/email/resetpassword';
     return require('./axiosRequest')(this.project_id, this.AUTH, this.HOSTNAME + method,
-      { receiver, to, resetUrl, lang })
+      { receiver, to, resetUrl, lang });
+  },
+  invite: function (receiver, to, emailFrom, linkUrl, lang) {
+    var method = '/apis/email/invite';
+    return require('./axiosRequest')(this.project_id, this.AUTH, this.HOSTNAME + method,
+      { receiver, to, emailFrom, linkUrl, lang });
+  },
+  changePermission: function (receiver, to, emailFrom, linkUrl, permissionFrom, permissionTo, lang) {
+    var method = '/apis/email/changePermission';
+    return require('./axiosRequest')(this.project_id, this.AUTH, this.HOSTNAME + method,
+      { receiver, to, emailFrom, linkUrl, permissionFrom, permissionTo, lang });
   }
 }
