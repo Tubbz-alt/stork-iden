773d7426b8c66092d80dbaaa7cac569d62d113c6
Phisit Kannasut <phisit@readyplanet.com>
2018-10-22 11:32:29 +0700

    edit readme and test

--- example/index.js
+++ example/index.js
@@ -8,47 +8,39 @@ const PROJECT = process.env.PROJECT || 'STORK';
 const APIKEY = process.env.APITOKEN || 'APIKEY';
 const stork = new STORK(PROJECT, APIKEY);
 
-var appId = 'APP_ID';
-var number = '+66636695587';
-var message = 'Send this message to mobile';
+/***
+ * FILE CONTAIL METHOD TEST
+ * */
+var tests = require('./testcase.json')
+
+var appId = '{APPLICATION ID}';
+
+/*** 
+ * Example for Sending SMS vis this package
+ * */
+var number = '{MOBILE NUMBER WITH INTERNATIONAL CODE eg. +66XXXXXXXXX}';
+var message = '{MESSAGE TO SEND}';
 var unicode = true;
-var sendSMS = false;
-var callbackUrl = 'https://storkgw.zense.online/alive'
-var refCode = 'fed';
-var otp = '66989';
-
-var tests = {
-  sendSMS: false,
-  OTPRequest: false,
-  OTPVerify: false,
-  PhoneRequest: false,
-  PhoneVerify: false,
-  emailValidator: false,
-  emailResetPassword: false,
-  emailInvitation: false,
-  emailChangePermission: false,
-  lineNotiSend: false
-}
-
-// Example for Sending SMS vis this package
 if (tests.sendSMS)
   stork.sendSMS(appId, number, message, unicode)
     .then(res => console.log('sendSMS', res))
     .catch(res => console.error('sendSMS error', res))
 
-// Example for OTP Solutions
-/*
-  Solutions
-  1. OTP Request : System return 'reference code' and Send OTP code to mobile
-  2. OTP Verify : System verify number reference and otp. if valid return 'true', else return 'false'
-
-  Parameter
-  - appId : Application Id
-  - number : Mobile number format +66614156789
-
-  Return
-  - refCode : reference code match with otp code (send to mobile)
+/***
+ * Example for OTP Solutions
+ * Solutions
+ * 1. OTP Request : System return 'reference code' and Send OTP code to mobile
+ * 2. OTP Verify : System verify number reference and otp. if valid return 'true', else return 'false'
+ * 
+ * Parameter
+ * - appId : Application Id
+ * - number : Mobile number international format
+ * 
+ * Return
+ * - refCode : reference code match with otp code (send to mobile)
 */
+var refCode = '{OTP REFERENCE CODE}';
+var otp = '{OTP CODE}';
 if (tests.OTPRequest)
   stork.OTPRequest(appId, number)
     .then(response_OTPRequest => {
@@ -63,18 +55,22 @@ if (tests.OTPVerify)
     })
     .catch(error => console.error('OTPVerify error', error))
 
-// Example for Phone Solutions
-/*
-  Solutions
-  1. Phone Request : Request for Phone verify
-  2. Phone Verify : for verify call-in
-
-  Parameter
-  - appId : Application Id
-  - number : Mobile number format +66614156789
-  - sendSMS : send SMS to remind call
-  - callbackUrl : url when phome call-in
-*/
+// 
+/***
+ * Example for Phone Solutions
+ * 
+ * Solutions
+ * 1. Phone Request : Request for Phone verify
+ * 2. Phone Verify : for verify call-in
+ * 
+ * Parameter
+ * - appId : Application Id
+ * - number : Mobile number with international format
+ * - sendSMS : send SMS to remind call
+ * - callbackUrl : url when phome call-in
+ * */
+var sendSMS = false;
+var callbackUrl = '{YOUR CALLBACK URL SEND WHEN COMPLETED}'
 if (tests.PhoneRequest)
   stork.PhoneRequest(appId, number, sendSMS, callbackUrl)
     .then(response_PhoneRequest => {
@@ -89,17 +85,19 @@ if (tests.PhoneVerify)
     })
     .catch(error => console.error('OTPVerify error', error))
 
-// Example for Email Validator
-/*
-    Parameter
-    - receiver : Name of receiver
-    - to : email of receiver
-    - validateUrl : url for veridate
-    - lang : language of mail
-*/
-const receiver = 'Receiver Name';
-const to = 'phisit@readyplanet.com';
-const validateUrl = 'https://auth.pointspot.co/validate/validatecodetobeverify';
+
+/***
+ * Example for Email Validator
+ * 
+ * Parameter
+ * - receiver : Name of receiver
+ * - to : email of receiver
+ * - validateUrl : url for veridate
+ * - lang : language of mail
+ * */
+const receiver = '{RECEIVER NAME}';
+const to = '{RECEIVER EMAIL}';
+const validateUrl = '{VALIDATE URL}';
 const lang = 'en'; // default
 if (tests.emailValidator)
   stork.emailValidator(receiver, to, validateUrl, lang)
@@ -108,15 +106,16 @@ if (tests.emailValidator)
     })
     .catch(error => console.error('emailValidator error', error))
 
-// Example for Email Reset Password
-/*
-    Parameter
-    - receiver : Name of receiver
-    - to : email of receiver
-    - validateUrl : url for reset password
-    - lang : language of mail
+/***
+ * Example for Email Reset Password
+ * 
+ * Parameter
+ * - receiver : Name of receiver
+ * - to : email of receiver
+ * - validateUrl : url for reset password
+ * - lang : language of mail
 */
-const resetUrl = 'https://auth.pointspot.co/resetpassword/tokencode';
+const resetUrl = '{RESET URL}';
 if (tests.emailResetPassword)
   stork.emailResetPassword(receiver, to, resetUrl, lang)
     .then(response_emailResetPassword => {
@@ -124,18 +123,19 @@ if (tests.emailResetPassword)
     })
     .catch(error => console.error('emailResetPassword error', error))
 
-// Example for Email Invitation
-/*
-    Parameter
-    - receiver : Name of receiver
-    - to : email of receiver
-    - emailFrom : Invitation from name
-    - inviteUrl : url for reset password
-    - lang : language of mail
-*/
-const emailFrom = 'Administrator';
-const inviteUrl = 'https://pointspot.co/invite/tokencode';
-const businessName = 'STORK';
+/***
+ * Example for Email Invitation
+ * 
+ * Parameter
+ * - receiver : Name of receiver
+ * - to : email of receiver
+ * - emailFrom : Invitation from name
+ * - inviteUrl : url for reset password
+ * - lang : language of mail
+ * */
+const emailFrom = '{GRANTER}';
+const inviteUrl = '{INVITE URL}';
+const businessName = '{BUSINESS NAME}';
 if (tests.emailInvitation)
   stork.invite(receiver, to, emailFrom, inviteUrl, businessName, lang)
     .then(response_emailInvitation => {
@@ -143,21 +143,21 @@ if (tests.emailInvitation)
     })
     .catch(error => console.error('emailInvitation error', error))
 
-// Example for Email change Permission
-/*
-    Parameter
-    - receiver : Name of receiver
-    - to : email of receiver
-    - emailFrom : Invitation from name
-    - linkUrl : url for reset password
-    - permissionFrom : before permission
-    - permissionTo : after permission
-    - lang : language of mail
-*/
-
-const linkUrl = 'https://pointspot.co';
-const permissionFrom = 'Editor';
-const permissionTo = 'Authorize';
+/***
+ * Example for Email change Permission
+ * 
+ * Parameter
+ * - receiver : Name of receiver
+ * - to : email of receiver
+ * - emailFrom : Invitation from name
+ * - linkUrl : url for reset password
+ * - permissionFrom : before permission
+ * - permissionTo : after permission
+ * - lang : language of mail
+ * */
+const linkUrl = '{LINK URL}';
+const permissionFrom = '{PERMISSION NAME}';
+const permissionTo = '{PERMISSION NAME}';
 if (tests.emailChangePermission)
   stork.changePermission(receiver, to, emailFrom, linkUrl, permissionFrom, permissionTo, businessName, lang)
     .then(response_emailChangePermission => {
@@ -165,15 +165,15 @@ if (tests.emailChangePermission)
     })
     .catch(error => console.error('emailChangePermission error', error))
 
-
-// Example for Line Noti to me
-/*
-    Parameter
-    - lineToken : Token chat room on line notify, follow this toget token https://notify-bot.line.me/en/
-    - lineMessage : Message will be send to line chat room
-*/
-const lineToken = '';
-const lineMessage = 'Example send line noti';
+/***
+ * Example for Line Noti to me
+ * 
+ * Parameter
+ * - lineToken : Token chat room on line notify, follow this toget token https://notify-bot.line.me/en/
+ * - lineMessage : Message will be send to line chat room
+ * */
+const lineToken = '{LINE NOTIFY TOKEN}';
+const lineMessage = '{MESSAGE TO SEND}';
 if (tests.lineNotiSend)
   stork.lineNotiSend(lineToken, lineMessage)
     .then(response_lineNotiSend => {
