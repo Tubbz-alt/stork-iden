1b9db4873353434b80660e70f3d55b1325764a5f
Phisit Kannasut <phisit@readyplanet.com>
2019-11-07 11:33:19 +0700

    Add comment language

--- stork.js
+++ stork.js
@@ -17,6 +17,12 @@ var STORK = function (project, token, debug) {
 
 STORK.prototype.sendSMS = require('./libs/sms').sendSMS;
 
+/**
+ * @method OTPRequest สำหรับการขอรหัส OTP
+ * @param {string} appId
+ * @param {string} number สำหรับใส่เบอร์มือถอืที่ต้องการ ในรูปแบบของ International format
+ * @param {string} lang ตัวเลือกภาษาสำหรับส่ง OTP Message เข้ามือถือ (Default 'en') ('en', 'th', 'vn', 'mm', 'lo', 'kh', 'jp', 'kr')
+ */
 STORK.prototype.OTPRequest = require('./libs/otp').OTPRequest;
 STORK.prototype.OTPVerify = require('./libs/otp').OTPVerify;
 
